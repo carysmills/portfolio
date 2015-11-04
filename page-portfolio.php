e344687faa4d08255b9fc0b8984705686b346b8a
Carys Mills <mills.carys@gmail.com>
2015-11-04 01:39:06 -0500

    updates to sections

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,8 +5,8 @@
   <?php get_sidebar(); ?>
 
   <div class="getMain">
-    <section id="about" class="container about">
 
+    <section id="about" class="about">
       <div class="bio content">
       
       <?php 
@@ -19,20 +19,37 @@
           <?php the_field(bio); ?>
        </div>
 
-    </div> <!-- end of bio content -->
+    </div> 
+</section> 
 
-</section> <!-- end of about section -->
+  <section class="portfolio">
+      <?php the_field(top); ?>
+        <p id="portfolio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
+  </section>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
+    <section class="expert" id="skills">
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quae tempora praesentium? Magni atque et maxime similique, quidem sit distinctio reprehenderit a labore ullam quae illum laborum reiciendis debitis ex?</p>
+    <?php the_field('expert_intro')?>
 
+      <div class="expertise">
+        <div class="expertContained"> <!-- this should be the flexbox -->
+        <?php while(has_sub_field('expert')): ?>
 
+          <div class="wrapp">
+            <div class="expertIcon">
+              <?php 
+              $image = get_sub_field('expert_icon');
+              if( !empty($image) ): ?>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+              <?php endif; ?>
+            </div>
 
+            <?php the_sub_field('expert_blurb');?>
+           <?php endwhile; ?>
+        </div>   
+        </div>  
+      </div>  
+  </section>
 
   </div> <!-- end getmain -->
 </div> <!-- end mainwrapper (sidebarwrap) for flexbox -->
